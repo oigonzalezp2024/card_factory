b7e4615bfb90e5e81bff2fee2c7370b8b73a990b
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2025-10-06 09:18:44 -0500

    fix(config): Mejora el acceso a los servicios.

--- control/vista/header.php
+++ control/vista/header.php
@@ -13,9 +13,9 @@
                 <li><a href="index.php">aliados</a></li>
                 <li><a href="fabricante.php">fabricante</a></li>
                 <li><a href="tarjetas.php">tarjetas</a></li>
-                <li><a href="http://localhost/web20251004/card_factory/services/imprimir/">generar tarjetas</a></li>
-                <li><a href="http://localhost/web20251004/card_factory/services/asentar/">asentar tarjetas</a></li>
-                <li><a href="http://localhost/web20251004/card_factory/services/reset/">resetar tarjetas</a></li>
+                <li><a href="../../services/imprimir/">generar tarjetas</a></li>
+                <li><a href="../../card_factory/services/asentar/">asentar tarjetas</a></li>
+                <li><a href="../../card_factory/services/reset/">resetar tarjetas</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><?php echo "<a href='../modelo/salir.php'><span class='glyphicon glyphicon-log-in'></span> Cerrar sesión </a>" ?></li>
